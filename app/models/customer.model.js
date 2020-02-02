a63ba82e381c8f5d43a09f2d0961a4d28c2d1c00
Dion Olympia <dionolympia@gmail.com>
2020-02-01 23:16:37 -0500

    First commit

---
+++ app/models/customer.model.js
@@ -0,0 +1,56 @@
+// Exported MySQL connection
+const sql = require("./db.js");
+
+// constructor for Customer object
+const Customer = function(customer){
+		this.email = customer.email;
+		this.name = customer.name;
+		this.active = customer.active;
+};
+
+// Create operation
+Customer.create = (newCustomer, result) => {
+	sql.query("INSERT INTO customers SET ?", newCustomer, (err,res)=> {
+		if (err) {
+			console.log("error: " err);
+			result(err, null);
+			return;
+		}
+		
+		console.log("created customer: ", { id: res.insertId, ...newCustomer});
+		result(null, { id: res.insertId, ...newCustomer });
+	});
+};
+
+// Read operation (1 customer by ID)
+Customer.findById = (customerId, result) => {
+	sql.query(`SELECT * FROM customers WHERE id = ${customerId}`, (err,res)=> {
+		if(err){
+			console.log("error: " err);
+			result(err, null);
+			return;
+		}
+		
+		// If results are not empty, show customer found
+		if(res.length){
+			console.log("Found customer: ", res[0]);
+			result(null, res[0]);
+			return;
+		}
+		
+		// If results ARE empty, then return not found
+		result({ kind:"not_found" }, null);
+	});
+};
+
+// Read operation (all customers)
+/*
+Customer.getAll = result => {
+	sql.query("SELECT * FROM customers", (err,res)=> {
+		if(err){
+			console.log("error: " err);
+			result(err, null)
+			*/
+			
+module.exports = Customer;
+
